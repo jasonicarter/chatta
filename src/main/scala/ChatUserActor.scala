6456c13d0638daf4a8971655a04610fd2555f3a3
Jason Carter <jason.ian.carter@gmail.com>
2016-08-15 22:36:04 -0400

    minor changes

--- src/main/scala/ChatUserActor.scala
+++ src/main/scala/ChatUserActor.scala
@@ -4,15 +4,15 @@
 
 import akka.actor._
 
-class ChatUserActor(val username: String, server: ActorRef) extends Actor {
+class ChatUserActor(val username: String, chatRoom: ActorRef) extends Actor {
 
-  server ! Connect(username) // send connect msg to serverActor
+  chatRoom ! Connect(username) // send connect msg to serverActor
 
   override def receive: Receive = {
     case NewMsg(from, msg) => {
       println(f"[$username%s's client] - $from%s: $msg%s")
     }
-    case Send(msg) => server ! Broadcast(msg)
+    case Send(msg) => chatRoom ! Broadcast(msg)
     case Info(msg) => {
       println(f"[$username%s's client] - $msg%s")
     }
