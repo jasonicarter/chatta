5ed236fd5eb82f9a3fe58b6f2f62d03f806bdb03
Jason Carter <jason.ian.carter@gmail.com>
2016-08-16 23:37:40 -0400

    Added akka streams Flow

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -2,13 +2,15 @@
   * Created by jasoncarter on 2016-08-11.
   */
 
-import akka.actor.ActorSystem
+import akka.NotUsed
+import akka.actor.{ActorSystem, PoisonPill, Props}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.ws.{Message, TextMessage}
-import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Flow
+import akka.stream.{ActorMaterializer, OverflowStrategy}
+import akka.stream.scaladsl.{Flow, Sink, Source}
 
 import scala.io.StdIn
+import scala.sys.process.ProcessBuilder.Source
 
 object Server extends App {
 
@@ -28,7 +30,11 @@ object Server extends App {
         complete("Hello websocket server.")
       }
     } ~
-    path("echo") {
+//    path("echo") {
+//      parameter('userName') { userName =>
+//        handleWebSocketMessages(echoMessage)
+//      }
+      path("echo") {
       get {
         handleWebSocketMessages(echoMessage)
       }
@@ -39,6 +45,39 @@ object Server extends App {
     case TextMessage.Strict(txt) => TextMessage("WS-ECHO: " + txt)
     case _ => TextMessage("Message type unsupported.")
   }
+
+
+  val chatRoom = actorSystem.actorOf(Props(new ChatRoomActor), "ChatRoom")
+
+// TODO: fix this using scalac.io example
+  def websocketChatFlow(user: String): Flow[Message, Message, _] = {
+
+    // each new connection is a new user
+    // TODO: good enough for 1st iteration, change later
+    val userActor = actorSystem.actorOf(Props(new ChatUserActor(user, chatRoom)))
+
+    // input flow, take Messages
+    val fromWebsocket: Sink[Message, _] = {
+      Flow[Message].map {
+        // transform to domain message
+        case TextMessage.Strict(text) => Send(text)
+      }.to(Sink.actorRef[Send](userActor, PoisonPill))
+    }
+
+    val toWebsocket: Source[Message, _] = {
+      Source.actorRef[Send](bufferSize = 10, OverflowStrategy.fail)
+        .mapMaterializedValue {
+          actor => userActor ! Send("source")
+            NotUsed
+        }.map {
+        (outMessage: Send) => TextMessage(outMessage, _)
+      }
+    }
+
+    Flow.fromSinkAndSource(fromWebsocket, toWebsocket)
+
+  }
+
 
   val binding = Http().bindAndHandle(route, interface, port)
   StdIn.readLine()
